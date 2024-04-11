bae55953dd9a3731c03a26810547d11128e41e44
EvgenyDavydenko <frontend82@mail.ru>
2024-04-11 13:06:20 +0300

    Install the vue-router package and create simple SPA

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Home Component</div>
+
+                    <div class="card-body">
+                        I'm an home component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';  
+    export default {
+        mounted() {
+            axios.get('/api/hello').then(r => {
+                console.log(r.data);
+            }).catch(error => {
+                console.log(error.response.data.errors);
+            })
+        }
+    }
+</script>
